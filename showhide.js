3431036d0b8ecf820b60a96a1832e11f55dcc03a
Alex <alexanderlperez@gmail.com>
2015-12-08 23:13:33 -0700

    saves across page loads

--- showhide.js
+++ showhide.js
@@ -12,25 +12,48 @@
 var State = {
     state: {},
 
+    saveListState: function (list) {
+        var listName = list.getAttribute('data-list');
+        var isHidden = list.getAttribute('data-ishidden') == "true";
+        var board = list.getAttribute('data-board');
+        var state = this.state;
+
+        state[board][listName] = {
+            ishidden: isHidden,
+        };
+    },
+
     saveBoard: function (board, lists) {
-        this.state[board] = {};
+        var state = this.state;
 
         forEach(lists, function (i, elList) {
             var list = elList.getAttribute('data-list');
-            var isHidden = elList.getAttribute('data-ishidden');
+            var isHidden = elList.getAttribute('data-ishidden') == "true";
 
-            this.state[board][list] = {
+            state[board][list] = {
                 ishidden: isHidden,
             };
-        }.bind(this));
+        });
     },
 
     syncFromLocalStorage: function () {
-        this.state = JSON.parse(localStorage.getItem('savedata'));
+        var state = JSON.parse(localStorage.getItem('savedata'));
+
+        if (!state) {
+            state = {};
+        }
+
+        this.state = state;
+
+        // change this to this.state 
+        return localStorage.getItem('savedata');
     },
 
     syncToLocalStorage: function () {
         localStorage.setItem('savedata', JSON.stringify(this.state));
+
+        // change this to this.state 
+        return localStorage.getItem('savedata');
     },
 };
 
@@ -39,9 +62,13 @@ var VM = {
         return document.querySelectorAll('.list');
     },
 
+    currentBoard: function () {
+        return window.location.href.split('/').filter(function (s, i, a) { return i == a.length-1; })[0];
+    },
+
     buildListMetadata: function (list) {
+        var board = this.currentBoard();
         var listName = list.querySelector('.list-header-name').textContent;
-        var board = window.location.href.split('/').filter(function (s, i, a) { return i == a.length-1; });
         var isHidden = list.querySelector('.list-cards').classList.contains('hide');
 
         list.setAttribute('data-list', listName);
@@ -55,7 +82,7 @@ var VM = {
         return {
             list: list.getAttribute('data-list'),
             board: list.getAttribute('data-board'),
-            isHidden: list.getAttribute('data-ishidden'),
+            ishidden: list.getAttribute('data-ishidden') == 'true',
         };
     },
 
@@ -78,19 +105,16 @@ var VM = {
     toggleList: function (list) {
         if (list.querySelector('.list-cards').classList.contains('hide')
             || list.querySelector('.open-card-composer').classList.contains('hide')) {
-            this.showList(list);
-        } else {
-            this.hideList(list);
-        }
+                this.showList(list);
+            } else {
+                this.hideList(list);
+            }
     },
 
-    appendListShowHide: function (list) {
+    appendListShowHideAndHandler: function (list, callback) {
         var icon = document.createElement('span');
         icon.className = 'show-hide icon-sm icon-remove dark-hover';
-
-        icon.addEventListener('click', function () {
-            this.toggleList(list);
-        }.bind(this));
+        icon.addEventListener('click', callback);
 
         list.querySelector('.list-header').appendChild(icon);
     },
@@ -99,20 +123,15 @@ var VM = {
         var icon = list.querySelector('.show-hide');
         try {
             icon.remove();
-        } catch (e) {
-            // happens on first-run
-            // console.log('No icon for', list);
-        }
+        } catch (e) { /* optimal solution, no need to complicate */}
     },
 
     applyListState: function (list, state) {
-        // get state of board
-        var data = this.getListMetadata(list);
-        var listState = state[data.board][data.list];
-
         // hide list if isHidden
-        if (data.isHidden) {
+        if (state) {
             this.hideList(list);
+        } else {
+            this.showList(list);
         }
     },
 };
@@ -121,37 +140,84 @@ var VM = {
 /** Main Entry Point**/
 
 
-// run when the site is loaded and after each board change
+// run when the site is loaded, after each board change
 window.CHECKINTERVAL = setInterval(doIfListsExist, 500);
+
+// check for clicks on generated menu items (may not exist yet)
 document.addEventListener('click', function (e) {
     if (e.target.className.indexOf('tile-link') > -1) {
+
+        console.log('moving to', e.target.textContent, State.state);
+
+        State.saveBoard(VM.lists()[0].getAttribute('data-board'), VM.lists());
+
+        console.log('syncTo:', State.syncToLocalStorage());
+
         window.CHECKINTERVAL = setInterval(doIfListsExist, 500);
     }
 });
+
+// have this match the beginning of the click handler
+// window.unload = function () {
+    // State.saveBoard(VM.lists()[0].getAttribute('data-board'), VM.lists());
+    // State.syncToLocalStorage();
+// }
 
 function doIfListsExist() {
     var lists = document.querySelectorAll('.list');
     if (lists.length > 0) {
         clearInterval(window.CHECKINTERVAL);           
 
-        // testing
-        testStateModel();
-        testVM();
-        // -
+        if (window.DEBUG) {
+            testStateModel();
+            testVM();
+            testSavingLoading();
+            return;
+        }
 
-        digest();
+        // get saved state
+        console.log('syncFrom:', State.syncFromLocalStorage(), State.state);
+
+        // create an entry for this board if one doesn't exist
+
+        console.log(State.State, State.state[VM.currentBoard()]);
+        if (!State.state[VM.currentBoard()]) {
+            console.log('creating new board entry');
+            State.state[VM.currentBoard()] = {};
+            console.log('State.state[VM.currentBoard()]', State.state[VM.currentBoard()]);
+        }
+
+
+        console.log('------- Syncing with %s -------', VM.currentBoard());
+        console.log('Synced, current state:', State.state);
+
+
+        // Lists: apply state, create the icons and events
+        forEach(VM.lists(), function (i, list) {
+            var listData = VM.buildListMetadata(list);
+
+            if (!State.state[VM.currentBoard()][listData.list]) {
+                State.state[VM.currentBoard()][listData.list] = {};
+            }
+
+            if (State.state[VM.currentBoard()][listData.list].ishidden == undefined) {
+                State.state[VM.currentBoard()][listData.list].ishidden = false;
+            }
+
+            VM.applyListState(list, State.state[VM.currentBoard()][listData.list].ishidden);
+ 
+            // prevent duplicate icons if reloading same board
+            VM.removeListShowHide(list);
+
+            // create the icons and set the click handler
+            VM.appendListShowHideAndHandler(list, function () {
+                VM.toggleList(list);
+                var listData = VM.buildListMetadata(list);
+                State.saveListState(list);
+                console.log(State.state[VM.currentBoard()], State.state[VM.currentBoard()][listData.list]);
+            });
+        });
     }
-}
-
-function digest() {
-
-    // create the icons
-    forEach(VM.lists(), function (i, list) {
-        VM.removeListShowHide(list);
-        VM.appendListShowHide(list);
-    })
-
-    // state handling
 }
 
 
@@ -185,10 +251,47 @@ function getSiblings(el, filter) {
 
 
 /* --- Tests --- */
- 
+
 
 function testStateModel() {
     console.group('Testing - State Model')
+
+    assert("saveListState", function () {
+        var list = document.querySelector('.list');
+
+        // hide a list 
+        list.querySelector('.list-cards').classList.add('hide');
+        list.querySelector('.open-card-composer').classList.add('hide');
+
+        // build the list metadata
+        var listName = list.querySelector('.list-header-name').textContent;
+        var board = window.location.href.split('/').filter(function (s, i, a) { return i == a.length-1; });
+        var isHidden = list.querySelector('.list-cards').classList.contains('hide');
+
+        list.setAttribute('data-list', listName);
+        list.setAttribute('data-board', board);
+        list.setAttribute('data-ishidden', isHidden);
+
+        // test our method with the list we set up
+        State.saveListState(list);
+
+        // check that the list state is saved in the State model correctly
+        if (!State.state[board][listName].ishidden)
+            return assert('', false, "ishidden isn't being set");
+
+        // clean up 
+        State.state = {};
+
+        list.removeAttribute('data-list');
+        list.removeAttribute('data-board');
+        list.removeAttribute('data-ishidden');
+
+        list.querySelector('.list-cards').classList.remove('hide');
+        list.querySelector('.open-card-composer').classList.remove('hide');
+
+        return true;
+
+    }, "list isn't being saved correctly");
 
     assert("saveBoard", function () {
         var board = window.location.href.split('/').filter(function (s, i, a) { return i == a.length-1; })[0];
@@ -212,10 +315,10 @@ function testStateModel() {
 
         // none of the lists should be ishidden
         for (var e in boardState) { if (boardState.hasOwnProperty(e)) {
-                if (boardState[e].ishidden == undefined) 
-                    return assert('', false, "ishidden field undefined");
-                if (boardState[e].ishidden == true)
-                    return assert('', false, "ishidden mysteriously true");
+            if (boardState[e].ishidden == undefined) 
+                return assert('', false, "ishidden field undefined");
+            if (boardState[e].ishidden == true)
+                return assert('', false, "ishidden mysteriously true");
         } }
 
         // clean up metadata
@@ -225,7 +328,7 @@ function testStateModel() {
 
         // clean up state
         State.state = {};
-        
+
         return true;
 
     }, "saveBoard isn't saving the board data correctly");
@@ -290,7 +393,7 @@ function testStateModel() {
         return true;
 
     }, "syncFromLocalStorage isn't retrieving correctly")
-    
+
     console.groupEnd();
 }
 
@@ -323,11 +426,11 @@ function testVM() {
         res = map(VM.lists(), function (i, list) {
             return VM.buildListMetadata(list);
         });
-        
+
         for (var i = 0; i < res.length; i++) {
             if (!res[i].list) return false;
             if (!res[i].board) return false;
-            if (res[i].isHidden == null || res[i].isHidden == undefined) 
+            if (res[i].ishidden == null || res[i].ishidden == undefined) 
                 return false;
         }
 
@@ -341,7 +444,7 @@ function testVM() {
 
         if (!list.querySelector('.list-cards').classList.contains('hide')
             || !list.querySelector('.open-card-composer').classList.contains('hide'))
-            return assert('', false, ".hide class not applied correctly");
+        return assert('', false, ".hide class not applied correctly");
 
         // cleanup - show the list again
         list.querySelector('.list-cards').classList.remove('hide');
@@ -362,22 +465,22 @@ function testVM() {
 
         if (!list.querySelector('.list-cards').classList.contains('hide')
             || !list.querySelector('.open-card-composer').classList.contains('hide'))
-            return assert('', false, ".hide class not applied correctly");
+        return assert('', false, ".hide class not applied correctly");
 
         VM.toggleList(list);
 
         if (list.querySelector('.list-cards').classList.contains('hide')
             || list.querySelector('.open-card-composer').classList.contains('hide'))
-            return assert('', false, ".hide class not removed correctly");
+        return assert('', false, ".hide class not removed correctly");
 
         return true;
 
     }, "toggleList doesn't toggle show/hide correctly");
 
-    assert("appendListShowHide", function () {
+    assert("appendListShowHideAndHandler", function () {
         var list = VM.lists()[0];
 
-        VM.appendListShowHide(list);
+        VM.appendListShowHideAndHandler(list);
 
         if (!list.querySelector('.show-hide'))
             return assert('', false, "icons aren't appearing");
@@ -387,12 +490,12 @@ function testVM() {
 
         return true;
 
-    }, "appendListShowHide isn't creating the icons correctly")
+    }, "appendListShowHideAndHandler isn't creating the icons correctly")
 
     assert("removeShowHide", function () {
         var list = VM.lists()[0];
 
-        VM.appendListShowHide(list);
+        VM.appendListShowHideAndHandler(list);
         VM.removeListShowHide(list);
 
         if (list.querySelector('.show-hide')) 
@@ -406,19 +509,16 @@ function testVM() {
         var curBoard = window.location.href.split('/').filter(function (s, i, a) { return i == a.length-1; });
         var list = VM.lists()[0];
         var listName = list.querySelector('.list-header-name').textContent;
-        var state = {};
-        state[curBoard] = {};
-        state[curBoard][listName] = {
-            ishidden: true
-        };
 
+        // make sure we start with a shown list
         VM.showList(list);
-        
-        VM.applyListState(list, state);
+
+        // this should hide the list
+        VM.applyListState(list, true);
 
         if (!list.querySelector('.list-cards').classList.contains('hide')
             || !list.querySelector('.open-card-composer').classList.contains('hide'))
-            return assert('', false, "list state isn't being applied and hiding the list");
+        return assert('', false, "list state isn't being applied, not hiding the list");
 
         // cleanup - show the list again
         list.querySelector('.list-cards').classList.remove('hide');
@@ -431,6 +531,13 @@ function testVM() {
     console.groupEnd();
 }
 
+function testSavingLoading() {
+    console.group("Testing - Saving and Loading");
+
+
+
+    console.end();
+}
 
 /* -- Assertion Functions-- */
 // TODO: make this into a standalone lib with grouping method that uses console.group
@@ -439,15 +546,14 @@ function assert(message, assertion, error) {
     var res = undefined;
 
     if (typeof assertion === 'function') {
-        try {
             res = assertion();
-        } catch (e) {
-            throw new Error('Test failed:\n' + e)
-        }
+            if (!res){
+                throw new Error('Test failed:\n' + e)
+            }
     } else {
         res = assertion;
     }
-    
+
     if (res) {
         console.log('%s %c✓', message, 'color: green;');
         return true;
